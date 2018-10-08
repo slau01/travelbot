79ce9c5f011ce9bd306a70bd0dd668fea10a177e
My Name <My Email>
2018-10-08 09:29:31 +0000

    Second commit

---
+++ TravelInsurnace/TravelInsurnace/getTravelInsuranceQuote.js
@@ -0,0 +1,63 @@
+'use strict'
+
+function getTravelInsuranceQuote(intentRequest, callback) {
+    const quote = Math.floor((Math.random() * 1000) + 1);
+    const tripType = intentRequest.currentIntent.slots.GetTripType;
+    const tripStartDate = intentRequest.currentIntent.slots.GetTripStartDate;
+    const tripReturnDate = intentRequest.currentIntent.slots.GetTripReturnDate;
+    const tripLocation = intentRequest.currentIntent.slots.GetTripLocation;
+    const tripTraveller = intentRequest.currentIntent.slots.GetTripTaveller;
+    const sessionAttributes = intentRequest.sessionAttributes;
+
+    /**
+        // Load confirmation history and track the current reservation.
+        const reservation = String(JSON.stringify({ ReservationType: 'Hotel', Location: location, RoomType: roomType, CheckInDate: checkInDate, Nights: nights }));
+        sessionAttributes.currentReservation = reservation;
+
+        if (intentRequest.invocationSource === 'DialogCodeHook') {
+            // Validate any slots which have been specified.  If any are invalid, re-elicit for their value
+            const validationResult = validateHotel(intentRequest.currentIntent.slots);
+            if (!validationResult.isValid) {
+                const slots = intentRequest.currentIntent.slots;
+                slots[`${validationResult.violatedSlot}`] = null;
+                callback(elicitSlot(sessionAttributes, intentRequest.currentIntent.name,
+                slots, validationResult.violatedSlot, validationResult.message));
+                return;
+            }
+
+            // Otherwise, let native DM rules determine how to elicit for slots and prompt for confirmation.  Pass price back in sessionAttributes once it can be calculated; otherwise clear any setting from sessionAttributes.
+            if (location && checkInDate && nights != null && roomType) {
+                // The price of the hotel has yet to be confirmed.
+                const price = generateHotelPrice(location, nights, roomType);
+                sessionAttributes.currentReservationPrice = price;
+            } else {
+                delete sessionAttributes.currentReservationPrice;
+            }
+            sessionAttributes.currentReservation = reservation;
+            callback(delegate(sessionAttributes, intentRequest.currentIntent.slots));
+            return;
+        }
+
+        // Booking the hotel.  In a real application, this would likely involve a call to a backend service.
+        console.log(`bookHotel under=${reservation}`);
+
+        delete sessionAttributes.currentReservationPrice;
+        delete sessionAttributes.currentReservation;
+        sessionAttributes.lastConfirmedReservation = reservation;
+    **/
+    var msg = "The quote of your trip to " + tripLocation + " from " + tripStartDate + " to " + tripReturnDate + " is HKD " + quote + ".";
+    let response = {
+        sessionAttributes: sessionAttributes,
+        dialogAction: {
+            type: "Close",
+            fulfillmentState: "Fulfilled",
+            message: {
+                contentType: "PlainText",
+                content: msg
+            }
+        }
+    }
+    callback(response);
+}
+
+module.exports = getTravelInsuranceQuote;
--- TravelInsurnace/TravelInsurnace/index.js
+++ TravelInsurnace/TravelInsurnace/index.js
@@ -1,5 +1,7 @@
 'use strict';
+const dispatch = require("./dispatch");
 
+/**
 exports.handler = (event, context, callback) => {
     // when intent get fulfilled, inform lex to complete the state
     console.log("EVENT= " + JSON.stringify(context));
@@ -22,4 +24,29 @@ exports.handler = (event, context, callback) => {
         }
     }
     callback(null, response);
+};
+**/
+
+
+
+
+
+
+
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on intent.
+// The JSON body of the request is provided in the event slot.
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.bot.name=${event.bot.name}`);
+        if (event.bot.name != 'BuyTravelInsurance') {
+            callback('Invalid Bot Name');
+        }
+        dispatch(event, (response) => callback(null, response));
+    }
+    catch (err) {
+        callback(err);
+    }
 };
